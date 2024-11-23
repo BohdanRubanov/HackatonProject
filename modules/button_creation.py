e3104aab25b0b4fe5b3ab9a1a844c92922de0a1a
Bohdan_Rubanov <bgbg78915@gmail.com>
2024-11-23 20:14:47 +0200

    added new window

--- modules/button_creation.py
+++ modules/button_creation.py
@@ -1,4 +1,12 @@
 import customtkinter as ctk
 class CustomButton(ctk.CTkButton):
-    def __init__(self, parent, text="", width=200, height=50, image=None, fg_color="transparent", hover = False, **kwargs):
-        super().__init__( master=parent, text=text, width=width,height=height, image=image, fg_color=fg_color, hover=hover, **kwargs)
+    def __init__(self, parent, text="", width=200, height=50, image=None, fg_color="transparent", hover = False, **kwargs) :
+        super().__init__( master=parent,
+                         text=text, 
+                         width=width,
+                         height=height, 
+                         image=image, 
+                         hover=hover,
+                         fg_color=fg_color, 
+                         border_width=0,
+                         **kwargs)
--- modules/main_menu.py
+++ modules/main_menu.py
@@ -2,6 +2,7 @@
 import customtkinter as ctk
 from modules.button_creation import CustomButton
 from modules.label_creation import CustomLabel
+from modules.new_windows import ToplevelWindow
 from PIL import Image, ImageTk
 
 # Создание констант с размерами окна
@@ -24,27 +25,42 @@ class App(ctk.CTk):
         self.resizable(False,False)
         # Указываем название окна
         self.title("makaki")
+        
+        self.main_frame = ctk.CTkFrame(self)
+        self.main_frame.pack(fill="both", expand=True)
          # Загрузка изображения для кнопки
-        self.button1 = CustomButton(self, text="", fg_color= "transparent", hover = False)
+        self.button1 = CustomButton(self.main_frame)
         self.button1.place(x= 530, y=670)
 
-        self.button2 = CustomButton(self, text="",  fg_color= "transparent", hover = False)
+        self.button2 = CustomButton(self.main_frame)
         self.button2.place(x= 770, y=670)
 
-        self.button3 = CustomButton(self, text="",  fg_color= "transparent", hover = False)
+        self.button3 = CustomButton(self.main_frame, command = self.new_window)
         self.button3.place(x= 1010, y=670)
+        
+        self.toplevel_window = None
 
         
         self.bg_image = Image.open("modules\\images\\Background.png")
         self.bg_image= self.bg_image.resize((self.winfo_width(), self.winfo_height()))
         self.bg_photo = ImageTk.PhotoImage(self.bg_image)
-        self.label1 = CustomLabel(self, image = self.bg_photo)
-        self.label1.place(relx=0, rely=0, relwidth = 1, relheight =1)
-
+        # self.label1 = CustomLabel(self, image = self.bg_photo, text="")
+        # self.label1.place(relx=0, rely=0, relwidth = 1, relheight =1)
+        
+        canvas = ctk.CTkCanvas(self.main_frame, width=600, height=400, highlightthickness=0)
+        canvas.pack(fill="both", expand=True)
+        # Установка изображения на Canvas
+        canvas.create_image(0, 0, image=self.bg_photo, anchor="nw")
+        
+        # self.attributes("-transparentcolor", "white")
+        
         # Полнимаем кнопки
         self.button1.lift()
         self.button2.lift()
         self.button3.lift()
+    def new_window(self):
+                self.toplevel_window = ToplevelWindow(self) 
+   
 
        
        
