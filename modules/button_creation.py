e7c8bc7a1df62166072f11a4ca662e9477d8901e
glib-pronin <0953044497gleb@gmail.com>
2024-11-23 20:14:23 +0200

    Images of buttons

--- modules/button_creation.py
+++ modules/button_creation.py
@@ -1,4 +1,4 @@
 import customtkinter as ctk
 class CustomButton(ctk.CTkButton):
-    def __init__(self, parent, text="", width=200, height=50, image=None, fg_color="transparent", hover = False, **kwargs):
-        super().__init__( master=parent, text=text, width=width,height=height, image=image, fg_color=fg_color, hover=hover, **kwargs)
+    def __init__(self, parent, text="", width=200, height=50, fg_color=None, hover = False, **kwargs):
+        super().__init__( master=parent, text=text, width=width,height=height, fg_color=fg_color, hover=hover, **kwargs)
